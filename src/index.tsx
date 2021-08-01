fd4caf301cebcaa75776f210d8a941f2c83d285d
dherv <damien.herv@gmail.com>
2021-08-01 15:09:24 +0900

    chore: FOC-6 add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ApolloProvider } from '@apollo/client';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import { client } from './apollo-client';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
+
+Sentry.init({
+  dsn: "https://03342846e14e46f6b8931dc194e780c5@o424697.ingest.sentry.io/5887800",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 function prepare() {
   if (process.env.NODE_ENV === "development") {
